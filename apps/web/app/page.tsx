6359bf8040ff4d9ea0a69028704c4de7acb53212
vladyslav <vladyslav.dmitriev@gmail.com>
2025-07-20 14:17:52 +0300

    remove log

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,6 @@
 import { Button } from '@/components/ui/button';
 
 export default function Home() {
-  console.log('test');
-
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello, Tailwind!</h1>
