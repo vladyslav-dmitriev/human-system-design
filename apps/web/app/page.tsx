bf7b085ab1a1e8eb4a678aa2083185379f8b0170
vladyslav <vladyslav.dmitriev@gmail.comgit>
2025-07-20 14:15:27 +0300

    test commin

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,8 @@
 import { Button } from '@/components/ui/button';
 
 export default function Home() {
+  console.log('test');
+
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello, Tailwind!</h1>
